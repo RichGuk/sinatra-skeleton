a67888f59b1f2e2bf7e3800d8c419489c3ebb680
Richard Grundy <rich@27smiles.com>
2009-09-03 22:07:45 +0100

    Added full path to the working dir so the database is created in the right location.

--- environment.rb
+++ environment.rb
@@ -7,7 +7,8 @@ require 'haml'
 
 require 'sinatra' unless defined?(Sinatra)
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3://development.db')
+# Database setup.
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{File.expand_path(File.dirname(__FILE__))}/development.db")
 
 # Load Models
 require 'models'
